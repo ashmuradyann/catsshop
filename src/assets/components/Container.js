f58ba6cf195075b360eab3bffeddfa8ec75b640f
ashmuradyann <ashot.muradyan16@gmail.com>
2022-06-18 16:36:31 +0300

    fix

--- src/assets/components/Container.js
+++ src/assets/components/Container.js
@@ -1,16 +1,13 @@
-import React, { useState } from 'react'
+import React from 'react'
 import Products from './Products'
 import Title from './Title'
 
 const Container = () => {
 
-  const [added, setAdded] = useState()
-
   return (
     <div className="container">
-        {added ? <div className="buy added">Added</div> : <div className="buy deleted">Deleted</div>}
         <Title value="Ты сегодня покормил кота?" />
-        <Products setAdded={setAdded} />
+        <Products />
     </div>
   )
 }
